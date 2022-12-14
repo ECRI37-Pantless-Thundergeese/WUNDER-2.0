a035e25eb3e4e4e38f98185eadd3fe04c4c1b03c
nathanycho <nathan.y.cho@gmail.com>
2022-12-14 14:46:44 -0500

    this commit will fix up server connections, allow CORS, and cookie parser

--- webpack.config.js
+++ webpack.config.js
@@ -22,9 +22,9 @@ module.exports = {
     },
 
     proxy: {
-      // '/': 'http://localhost:3000',
+      '/': 'http://localhost:3000',
       '/login': 'http://localhost:3000',
-      '/user' : 'http://localhost:3000',
+      '/home/user': 'http://localhost:3000',
     },
   },
   plugins: [
