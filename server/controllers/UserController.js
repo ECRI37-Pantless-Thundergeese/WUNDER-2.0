e6a72a38378ec2d4cb819f9fdf9a0b0a6f56c272
kolashah <105250729+kolashah@users.noreply.github.com>
2022-12-15 12:24:33 -0500

    modularized addpark

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -4,6 +4,44 @@ const bcrypt = require('bcryptjs');
 
 const userController = {};
 
+userController.verifyUser = (req, res, next) => {
+  const { password, username } = req.body;
+  console.log('inside verifyUser');
+  User.findOne({ username: `${username}` }).then((doc) => {
+    // if username doesn't exist, send them to sign up
+    if (!doc) {
+      return res.redirect('/signup');
+    }
+    // check password
+    bcrypt
+      .compare(password, doc.password)
+      .then((result) => {
+        if (!result) {
+          console.log('incorrect username or password');
+          return res.redirect('/');
+        } else {
+          res.locals.userID = doc._id;
+          // console.log('should log ID :', doc._id);
+          return next();
+        }
+      })
+      .catch((err) => {
+        return next({
+          log: 'Caught error while verifying password in verifyUser middleware',
+          status: 400,
+          message: { err: 'An unknown error occured.' },
+        });
+      })
+      .catch((err) => {
+        return next({
+          log: 'Caught error while verifying username in verifyUser middleware',
+          status: 400,
+          message: { err: 'An unknown error occured.' },
+        });
+      });
+  });
+};
+
 // Create a new user in the database
 userController.createUser = (req, res, next) => {
   // console.log('req.body :', req.body);
@@ -26,7 +64,7 @@ userController.getUser = (req, res, next) => {
 
   User.findOne({ _id: ssid })
     .then((userData) => {
-      console.log(JSON.stringify(userData, null))
+      // console.log(JSON.stringify(userData, null))
       res.locals.userData = userData; // <-- send back all user data
       return next();
     })
@@ -37,28 +75,38 @@ userController.getUser = (req, res, next) => {
 };
 
 // Add a park to a user's completed parks
-userController.addPark = async (req, res, next) => {
-  try {
-    const parkCode = req.params.parkCode;
-    const newPark = {
-      date: req.body.date,
-      notes: req.body.notes,
-      activitiesCompleted: req.body.activitiesDone,
-    };
-
-    const user = await User.findOne({ name: req.body.username})
-    if (username) {
-      const parksVisited = { ...user.parksVisited, [parkCode]: newPark };
-      user.parksVisited = parksVisited;
-      const newUser = await user.save();
-      console.log('newuser :', newUser);
-    }
-    res.locals.park = user.parksVisited[parkCode]; // <-- send back the newly added park's info
-    return next();
-  } catch (err) {
-    return next({err: 'cannot add park, user not found'});
-  }
+userController.addPark = (req, res, next) => {
+  const { ssid } = req.cookies;
+  User.findOneAndUpdate({ _id: ssid }, { parksVisited: req.body })
+    .then((user) => {
+      res.locals.park = user;
+      return next();
+    })
+    .catch((err) => next(err));
 };
+//   console.log('addPark controller accessed');
+//   try {
+//     const parkCode = req.params.parkCode;
+//     console.log(req.body);
+
+//     const newPark = {
+//       visitsArr: [{ date: req.body.date, notes: req.body.notes }],
+//       activitiesCompleted: req.body.activitiesDone,
+//     };
+
+//     const user = await User.findOne({ name: req.body.username });
+//     if (username) {
+//       const parksVisited = { ...user.parksVisited, [parkCode]: newPark };
+//       user.parksVisited = parksVisited;
+//       const newUser = await user.save();
+//       console.log('newuser :', newUser);
+//     }
+//     res.locals.park = user.parksVisited[parkCode]; // <-- send back the newly added park's info
+//     return next();
+//   } catch (err) {
+//     return next({ err: 'cannot add park, user not found' });
+//   }
+// };
 
 // Get parks completed array for icon coloring on landing page
 // userController.getParks = (req, res, next) => {
@@ -89,43 +137,5 @@ userController.addPark = async (req, res, next) => {
 //     return next(err);
 //   }
 // };
-
-userController.verifyUser = (req, res, next) => {
-  const { password, username } = req.body;
-  console.log('inside verifyUser');
-  User.findOne({ username: `${username}` }).then((doc) => {
-    // if username doesn't exist, send them to sign up
-    if (!doc) {
-      return res.redirect('/signup');
-    }
-    // check password
-    bcrypt
-      .compare(password, doc.password)
-      .then((result) => {
-        if (!result) {
-          console.log('incorrect username or password');
-          return res.redirect('/');
-        } else {
-          res.locals.userID = doc._id;
-          console.log('should log ID :', doc._id);
-          return next();
-        }
-      })
-      .catch((err) => {
-        return next({
-          log: 'Caught error while verifying password in verifyUser middleware',
-          status: 400,
-          message: { err: 'An unknown error occured.' },
-        });
-      })
-      .catch((err) => {
-        return next({
-          log: 'Caught error while verifying username in verifyUser middleware',
-          status: 400,
-          message: { err: 'An unknown error occured.' },
-        });
-      });
-  });
-};
 
 module.exports = userController;
